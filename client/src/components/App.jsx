c7341ba2927ee58c3d1c93ef691c49be2a2eb754
Sanuja Methmal <methmal66@yahoo.com>
2023-05-25 21:50:26 +0530

    intergrate react router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,17 @@
 import "../styles/App.css";
 import Register from "./Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="App">
-            <Register />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
 
 export default App;
-
-//TODO - Create user register compoenent
