3719e7dc9554c1b66f492fc9cac40364b365fe20
Sanuja Methmal <methmal66@yahoo.com>
2023-05-31 17:57:41 +0530

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,25 @@
-import "../styles/Header.css";
 import Avatar from "./Avatar";
+import styled from "styled-components";
+
+const Div = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    height: 50px;
+    width: 100%;
+    background-color: #aaaaaa;
+    color: white;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: "Roboto", sans-serif;
+`;
 
 const Header = () => {
     return (
-        <div className="header">
+        <Div>
             <Avatar />
-        </div>
+        </Div>
     );
 };
 
